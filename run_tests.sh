677c87a18ea58c35b4115a1204fca39eb6abbe17
Aaron Gorka <aaron.gorka@cmdsolutions.com.au>
2019-11-04 15:29:59 +1100

    Fix tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,9 +3,6 @@
 set -euxo pipefail
 
 kubectl --help
-aws-iam-authenticator --help
 helm --help
-helm tiller --help
 #Cant test helm diff as it requires a kubernetes cluster
 aws --version
-velero --help
